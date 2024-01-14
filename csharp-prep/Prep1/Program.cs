5d41a9306850bc1f2f53e1886f9f7eb2cce583dc
scan4rod <scan4rod@hotmail.com>
2024-01-14 01:25:40 -0400

    week 1 done!

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What's your first name? ");
+        string fname = Console.ReadLine();
+        Console.Write("What's your last name? ");
+        string lname = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+
     }
 }
